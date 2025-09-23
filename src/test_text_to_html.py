6a79908037e172d79d805a2494b06db51c75f379
MattD <mahavirdalal@gmail.com>
2025-09-24 02:22:09 +0530

    CH2L6 - feat: convert TextNode to LeafNode with text_node_to_html_node and tests

---
+++ src/test_text_to_html.py
@@ -0,0 +1,52 @@
+import unittest
+from textnode import TextNode, TextType
+from text_to_html import text_node_to_html_node
+
+
+class TestTextToHtml(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold!", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold!")
+
+    def test_italic(self):
+        node = TextNode("Italic!", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic!")
+
+    def test_code(self):
+        node = TextNode("x = 5", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "x = 5")
+
+    def test_link(self):
+        node = TextNode("Boot.dev", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Boot.dev")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
+
+    def test_image(self):
+        node = TextNode("An image", TextType.IMAGE, "https://img.com/pic.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://img.com/pic.png", "alt": "An image"})
+
+    def test_invalid_type_raises(self):
+        node = TextNode("oops", "unknown")  # not a TextType
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
+
+if __name__ == "__main__":
+    unittest.main()
